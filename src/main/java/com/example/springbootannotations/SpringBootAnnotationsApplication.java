664080db87c480616810a0b50e94d331a61fbc28
Harsh <harshgandharav680@gmail.com>
2022-09-14 08:44:40 +0530

    services

---
+++ src/main/java/com/example/springbootannotations/SpringBootAnnotationsApplication.java
@@ -0,0 +1,14 @@
+package com.example.springbootannotations;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootAnnotationsApplication {
+
+  public static void main(final String[] args) {
+
+    SpringApplication.run(SpringBootAnnotationsApplication.class, args);
+  }
+
+}
